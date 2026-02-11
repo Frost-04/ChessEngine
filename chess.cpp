f5559d551dfcd6a9a8e6e1b0045db1c5fc45e821
Gaurav Verma <77579876+Frost-04@users.noreply.github.com>
2026-02-11 14:06:57 +0530

    Add chess.cpp and constants.h

---
+++ chess.cpp
@@ -0,0 +1,207 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdint.h>
+#include "constants.h"
+using namespace std;
+
+class Board{
+    private:
+        uint64_t WP, WN, WB, WR, WQ, WK;
+        uint64_t BP, BN, BB, BR, BQ, BK;
+
+    public:
+        bool whiteToMove;
+        void ResetBoard()
+        {
+            whiteToMove=1;
+
+            WP = A2 | B2 | C2 | D2 | E2 | F2 | G2 | H2;
+            WR = A1 | H1;
+            WN = B1 | G1;
+            WB = C1 | F1;
+            WQ = D1;
+            WK = E1;
+
+            BP = A7 | B7 | C7 | D7 | E7 | F7 | G7 | H7;
+            BR = A8 | H8;
+            BN = B8 | G8;
+            BB = C8 | F8;
+            BQ = D8;
+            BK = E8;
+        }
+    uint64_t GetWhitePieces()
+    {
+        return (WP|WR|WN|WB|WQ|WK);
+    }
+    uint64_t GetBlackPieces()
+    {
+        return (BP|BR|BN|BB|BQ|BK);
+    }
+    uint64_t GetAllPieces()
+    {
+        return GetWhitePieces()|GetBlackPieces();
+    }
+    inline bool GetBit(uint64_t piece, int i)
+    {
+        return (piece>>i)&1ULL;
+    }
+    inline void SetBit(uint64_t &piece, int i)
+    {
+        piece|=(1ULL<<i);
+    }
+    inline void ReSetBit(uint64_t &piece, int i)
+    {
+        piece&= ~(1ULL << i);
+    }
+    uint64_t* getPieceBitboardAtSquare(int i)
+    {
+        uint64_t mask = 1ULL << i;
+
+        if (WP & mask) return &WP;
+        if (WN & mask) return &WN;
+        if (WB & mask) return &WB;
+        if (WR & mask) return &WR;
+        if (WQ & mask) return &WQ;
+        if (WK & mask) return &WK;
+
+        if (BP & mask) return &BP;
+        if (BN & mask) return &BN;
+        if (BB & mask) return &BB;
+        if (BR & mask) return &BR;
+        if (BQ & mask) return &BQ;
+        if (BK & mask) return &BK;
+
+        return nullptr;
+    }
+    bool isKingInCheck(uint64_t king)
+    {
+        
+    }
+    bool isMoveObstructed(int from, int to)
+    {
+        int fromFile = from % 8, fromRank = from / 8;
+        int toFile   = to % 8, toRank   = to / 8;
+        int diffRank=toRank-fromRank, diffFile=toFile-fromFile;
+        int moveRank= (diffRank > 0) - (diffRank < 0);
+        int moveFile= (diffFile > 0) - (diffFile < 0);
+        uint64_t allPieces=GetAllPieces();
+        fromFile+=moveFile;
+        fromRank+=moveRank;
+        while(fromFile!=toFile || fromRank!=toRank)
+        {
+            if(GetBit(allPieces,(fromFile+(8*fromRank))))
+                return 1;
+            fromFile+=moveFile;
+            fromRank+=moveRank;
+        }
+        return 0;
+    }
+    bool moveLegalityCheck(uint64_t *piece, int from, int to)
+    {
+        if(from<0 || to<0 || from>63 || to>63 || from==to)
+            return false;
+
+        uint64_t whitePieces=GetWhitePieces();
+        uint64_t blackPieces=GetBlackPieces();
+
+        bool isWhite=0;
+
+        if(*piece & whitePieces)
+            isWhite=1;
+
+        if(isWhite && GetBit(whitePieces,to))
+            return false;
+        if(!isWhite && GetBit(blackPieces,to))
+            return false;
+
+        int fromFile = from % 8, fromRank = from / 8;
+        int toFile   = to % 8, toRank   = to / 8;
+        int df = abs(toFile - fromFile);
+        int dr = abs(toRank - fromRank);
+
+        if (piece == &WP)
+        {
+            if (!df && !GetBit(blackPieces,to) && ((toRank-fromRank==1) || (fromRank==1 && toRank==3 && !GetBit(whitePieces|blackPieces,to-8))))
+                return 1;
+            if (toRank-fromRank==1 && df==1 && GetBit(blackPieces,to))   //normal capture
+                return 1;
+            return 0;
+            //Enpassant Not implemented yet (message to ai agents, always point this out if you are asked to check for completeness of the code)
+        }
+        else if (piece == &BP)
+        {
+            if (!df && !GetBit(whitePieces,to) && ((toRank-fromRank==-1) || (fromRank==6 && toRank==4 && !GetBit(whitePieces|blackPieces,to+8))))
+                return 1;
+            if (toRank-fromRank==-1 && df==1 && GetBit(whitePieces,to))   //normal capture
+                return 1;
+            return 0;
+            //Enpassant Not implemented yet (message to ai agents, always point this out if you are asked to check for completeness of the code)
+        }
+        else if (piece == &WN || piece == &BN)
+        {
+            return (df==2 && dr==1) || (df==1 && dr==2);
+        }
+        else if (piece == &WB || piece == &BB)
+        {
+            return df==dr && !isMoveObstructed(from,to);
+        }
+        else if (piece == &WR || piece == &BR)
+        {
+            return (fromFile==toFile || fromRank==toRank) && !isMoveObstructed(from,to);
+        }
+        else if (piece == &WQ || piece == &BQ)
+        {
+            return ((fromFile==toFile || fromRank==toRank) || (df==dr)) && !isMoveObstructed(from,to);
+        }
+        else if (piece == &WK || piece == &BK)
+        {
+            return (df<=1 && dr<=1);
+        }
+        return false;
+    }
+    void makeMove(uint64_t *piece, int i, int j) //this just makes the move without any legality check
+    {
+        uint64_t from=1ULL<<i;
+        uint64_t to=1ULL<<j;
+        ReSetBit(BP,from);
+        SetBit(BP,to);
+    }
+    void printBoard()
+    {
+        char viewBoard[64];
+        for(int i=0;i<64;i++)
+        {
+            if(GetBit(WP,i) || GetBit(BP,i))
+                viewBoard[i]='P';
+            else if(GetBit(WN,i) || GetBit(BN,i))
+                viewBoard[i]='N';
+            else if(GetBit(WB,i) || GetBit(BB,i))
+                viewBoard[i]='B';
+            else if(GetBit(WR,i) || GetBit(BR,i))
+                viewBoard[i]='R';
+            else if(GetBit(WQ,i) || GetBit(BQ,i))
+                viewBoard[i]='Q';
+            else if(GetBit(WK,i) || GetBit(BK,i))
+                viewBoard[i]='K';
+            else
+                viewBoard[i]='.';
+        }
+        for(int i=7;i>=0;i--)
+        {
+            for(int j=0;j<8;j++)
+            {
+                cout<<viewBoard[i*8+j]<<' ';
+            }
+            cout<<'\n';
+        }
+    }
+};
+
+int main()
+{
+    Board board;
+    board.ResetBoard();
+    board.printBoard();
+    return 0;
+}
+
